c699c7cfe66be1ea4b020a237be90b7774467979
rajas <rajashekarraju123@gmail.com>
2020-06-12 23:30:05 +0530

    SimpleFragment which opens simple webview in fragment taking whole space

---
+++ app/src/main/java/com/developersbreach/webviewandroid/simple/SimpleWebViewFragment.kt
@@ -0,0 +1,27 @@
+package com.developersbreach.webviewandroid.simple
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.developersbreach.webviewandroid.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SimpleWebViewFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SimpleWebViewFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_simple_web_view, container, false)
+    }
+}
