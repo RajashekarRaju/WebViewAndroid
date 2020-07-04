e583f26a5c8b1d40bc2a35d60b213b0d4fa056fc
Rajasekhar <rajashekarraju123@gmail.com>
2020-07-04 23:56:40 +0530

    Added CardViews to navigate between fragments

--- app/src/main/java/com/developersbreach/webviewandroid/MainFragment.kt
+++ app/src/main/java/com/developersbreach/webviewandroid/MainFragment.kt
@@ -5,15 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.developersbreach.webviewandroid.simple.SimpleWebViewFragment
+import com.google.android.material.card.MaterialCardView
 
-/**
- * A simple [Fragment] subclass.
- */
-class HomeFragment : Fragment() {
+
+class MainFragment : Fragment() {
 
     companion object {
-        fun newInstance() = HomeFragment()
+        fun newInstance() = MainFragment()
     }
 
     override fun onCreateView(
@@ -22,5 +23,25 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<MaterialCardView>(R.id.simple_fragment_card_view).setOnClickListener {
+            val fragment = SimpleWebViewFragment.newInstance()
+            val fragmentManager: FragmentManager = childFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.main_fragment_container, fragment)
+            fragmentTransaction.commit()
+        }
+
+        view.findViewById<MaterialCardView>(R.id.interactive_fragment_card_view).setOnClickListener {
+            val fragment = SimpleWebViewFragment.newInstance()
+            val fragmentManager: FragmentManager = childFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.main_fragment_container, fragment)
+            fragmentTransaction.commit()
+        }
     }
 }
