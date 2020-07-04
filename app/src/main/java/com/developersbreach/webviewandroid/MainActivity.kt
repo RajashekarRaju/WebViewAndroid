77e7fabf97ca1f71ff2e828654f6d9131671652d
Rajasekhar <rajashekarraju123@gmail.com>
2020-07-04 23:57:38 +0530

    Show MainFragment as default to appear in MainActivity.kt

--- app/src/main/java/com/developersbreach/webviewandroid/MainActivity.kt
+++ app/src/main/java/com/developersbreach/webviewandroid/MainActivity.kt
@@ -2,10 +2,19 @@ package com.developersbreach.webviewandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainFragment = MainFragment()
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragment_container, mainFragment)
+        fragmentTransaction.commit()
     }
 }
