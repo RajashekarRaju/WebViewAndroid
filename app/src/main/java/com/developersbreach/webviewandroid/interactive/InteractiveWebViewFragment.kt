939fcb409fe1f04e96b75866952089aca449ba99
Rajasekhar <rajashekarraju123@gmail.com>
2020-07-06 23:59:10 +0530

    WebView interaction enabled JavaScript, WebSettings

--- app/src/main/java/com/developersbreach/webviewandroid/interactive/InteractiveWebViewFragment.kt
+++ app/src/main/java/com/developersbreach/webviewandroid/interactive/InteractiveWebViewFragment.kt
@@ -1,21 +1,31 @@
 package com.developersbreach.webviewandroid.interactive
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.Toolbar
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import androidx.lifecycle.ViewModelProvider
 import com.developersbreach.webviewandroid.R
+import com.google.android.material.bottomappbar.BottomAppBar
 
 class InteractiveWebViewFragment : Fragment() {
+
+    private lateinit var bottomAppBar: BottomAppBar
+    private lateinit var webView: WebView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var webSettings: WebSettings
+    private lateinit var viewModel: InteractiveWebViewViewModel
 
     companion object {
         fun newInstance() = InteractiveWebViewFragment()
     }
-
-    private lateinit var viewModel: InteractiveWebViewViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,18 +34,35 @@ class InteractiveWebViewFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_interactive_web_view, container, false)
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Toolbar>(R.id.toolbar_interactive_fragment).setNavigationOnClickListener {
-            requireActivity().onBackPressed()
-        }
+        bottomAppBar = view.findViewById(R.id.web_view_bottom_app_bar)
+        webView = view.findViewById(R.id.interactive_web_view)
+        progressBar = view.findViewById(R.id.progress_bar_interactive_web)
+
+        webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(InteractiveWebViewViewModel::class.java)
 
+        viewModel = ViewModelProvider(this).get(InteractiveWebViewViewModel::class.java)
+        webView.loadUrl(viewModel.urlLink)
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                progressBar.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                progressBar.visibility = View.GONE
+            }
+        }
     }
 
 }
--- app/src/main/java/com/developersbreach/webviewandroid/interactive/InteractiveWebViewViewModel.kt
+++ app/src/main/java/com/developersbreach/webviewandroid/interactive/InteractiveWebViewViewModel.kt
@@ -1,8 +1,15 @@
 package com.developersbreach.webviewandroid.interactive
 
+
 import androidx.lifecycle.ViewModel
 
 class InteractiveWebViewViewModel : ViewModel() {
 
-    // Interactive WebView
+    private var _urlLink = String()
+    val urlLink: String
+        get() = _urlLink
+
+    init {
+        _urlLink = "https://github.com/RajashekarRaju"
+    }
 }
