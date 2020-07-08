2f6ba4875df56c48f0aec3fc28eeadd336d2050e
Rajasekhar <rajashekarraju123@gmail.com>
2020-07-08 20:56:01 +0530

    Search previous, next, refresh, clear matches, theme added these interfaces

--- app/src/main/java/com/developersbreach/webviewandroid/interactive/InteractiveWebViewFragment.kt
+++ app/src/main/java/com/developersbreach/webviewandroid/interactive/InteractiveWebViewFragment.kt
@@ -4,18 +4,21 @@ import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
 import android.widget.ProgressBar
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
 import com.developersbreach.webviewandroid.R
 import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
 
 class InteractiveWebViewFragment : Fragment() {
 
@@ -49,17 +52,11 @@ class InteractiveWebViewFragment : Fragment() {
         setWebViewMenu(bottomAppBar)
     }
 
-    private fun setWebViewMenu(bottomAppBar: BottomAppBar) {
-        bottomAppBar.replaceMenu(R.menu.bottom_app_bar_menu)
-        bottomAppBar.hideOnScroll = true
-        invalidateMenuOptions()
-    }
-
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         viewModel = ViewModelProvider(this).get(InteractiveWebViewViewModel::class.java)
-        webView.loadUrl(viewModel.urlLink)
+        loadWebView()
 
         webView.webViewClient = object : WebViewClient() {
             override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
@@ -74,8 +71,110 @@ class InteractiveWebViewFragment : Fragment() {
         }
     }
 
+    private fun setWebViewMenu(bottomAppBar: BottomAppBar) {
+        bottomAppBar.replaceMenu(R.menu.bottom_app_bar_menu)
+        bottomAppBar.setOnMenuItemClickListener(onWebMenuClickListener())
+        bottomAppBar.hideOnScroll = true
+        invalidateMenuOptions()
+    }
+
+    private fun onWebMenuClickListener(): Toolbar.OnMenuItemClickListener {
+        return object : MenuItem.OnMenuItemClickListener,
+            Toolbar.OnMenuItemClickListener {
+            @SuppressLint("NewApi")
+            override fun onMenuItemClick(item: MenuItem): Boolean {
+                when (item.itemId) {
+                    R.id.close_fragment_menu_item -> closeWebView()
+                    R.id.refresh_page_detail_fragment_menu_item -> loadWebView()
+                    R.id.search_detail_content_menu_item -> searchWebView()
+                    R.id.change_theme_detail_fragment_menu_item -> changeWebViewTheme()
+                    R.id.find_top_web_view_menu_item -> findPreviousQuery()
+                    R.id.find_bottom_web_view_menu_item -> findNextQuery()
+                    R.id.clear_matches_web_view_menu_item -> clearMatchesAndSwitchMenu()
+                }
+                return true
+            }
+        }
+    }
+
+    private fun closeWebView() {
+        requireActivity().onBackPressed()
+    }
+
+    private fun loadWebView() {
+        webView.loadUrl(viewModel.urlLink)
+    }
+
+    private fun searchWebView() {
+        val dialog: AlertDialog =
+            MaterialAlertDialogBuilder(context, R.style.MaterialDialog)
+                .setTitle(R.string.search_query_dialog_title)
+                .setView(R.layout.search_edit_text)
+                .setPositiveButton(R.string.search_query_dialog_positive_button, null)
+                .setNegativeButton(R.string.search_query_dialog_negative_button, null)
+                .create()
+
+        dialog.setOnShowListener { alertDialog ->
+            alertDialog as AlertDialog
+            val positiveButton: Button = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            val negativeButton: Button = alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+            val editText: TextInputEditText = alertDialog.findViewById(R.id.text_input_layout)!!
+
+            positiveButton.setOnClickListener {
+                val query: String = editText.text.toString()
+                if (query.isEmpty()) {
+                    editText.error = getString(R.string.search_query_error_message)
+                } else {
+                    webView.findAllAsync(query)
+                    dialog.dismiss()
+                    bottomAppBar.replaceMenu(R.menu.web_view_menu)
+                }
+            }
+
+            negativeButton.setOnClickListener {
+                alertDialog.dismiss()
+            }
+        }
+        dialog.show()
+        setDialogGravity(dialog)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    var themeMode = WebSettings.FORCE_DARK_OFF
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun changeWebViewTheme() {
+        if (themeMode == WebSettings.FORCE_DARK_ON) {
+            webSettings.forceDark = WebSettings.FORCE_DARK_OFF
+            themeMode = WebSettings.FORCE_DARK_OFF
+        } else if (themeMode == WebSettings.FORCE_DARK_OFF) {
+            webSettings.forceDark = WebSettings.FORCE_DARK_ON
+            themeMode = WebSettings.FORCE_DARK_ON
+        }
+    }
+
+    private fun findPreviousQuery() {
+        webView.findNext(false)
+    }
+
+    private fun findNextQuery() {
+        webView.findNext(true)
+    }
+
+    private fun clearMatchesAndSwitchMenu() {
+        webView.clearMatches()
+        bottomAppBar.replaceMenu(R.menu.bottom_app_bar_menu)
+        invalidateMenuOptions()
+    }
+
     private fun invalidateMenuOptions() {
         val item: MenuItem = bottomAppBar.menu.getItem(3)
         item.isVisible = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+    }
+
+    private fun setDialogGravity(dialog: AlertDialog) {
+        val params = dialog.window?.attributes
+        params!!.gravity = Gravity.TOP
+        dialog.window!!.attributes = params
     }
 }
